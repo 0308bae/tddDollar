67200d378f9c003f49f3c283f0ffbc5243f97b68
JaeikBae <0308bae@gmail.com>
2022-07-27 17:33:27 +0900

    chap1 end.

--- src/Sum.java
+++ src/Sum.java
@@ -1,14 +1,22 @@
 public class Sum implements Expression{
-    Money augend;
-    Money addend;
+    Expression augend;
+    Expression addend;
 
-    Sum(Money augend, Money addend){
+    Sum(Expression augend, Expression addend){
         this.addend = addend;
         this.augend = augend;
     }
 
     public Money reduce(Bank bank, String to){
-        int amount = this.augend.amount + this.addend.amount;
+        int amount = this.augend.reduce(bank, to).amount + this.addend.reduce(bank, to).amount;
         return new Money(amount, to);
+    }
+
+    public Expression plus(Expression addend){
+        return new Sum(this, addend);
+    }
+
+    public Expression times(int multiplier){
+        return new Sum(augend.times(multiplier), addend.times(multiplier));
     }
 }
